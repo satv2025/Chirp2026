c59454950fe7be4d56a9db727278d32597042932
satv2025 <traplegionrecords2020@gmail.com>
2026-06-22 20:32:45 -0300

    act

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,12 +1,13 @@
 window.CHIRP = {
-  SUPABASE_URL: "https://kdohnkpykpcmnoyxhkte.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtkb2hua3B5a3BjbW5veXhoa3RlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwODYxMjgsImV4cCI6MjA5NzY2MjEyOH0.kPxH259iv3RZZxPJWIpqnfwzyRN-DJoXZdf9uNkRO7c",
-  authCallback: window.location.origin + "/auth-callback.html",
-  updatePassword: window.location.origin + "/update-password.html",
-  chirpyEndpoint: "https://db.chirp.com.ar/functions/v1/chirpy",
-  mediaBucket: "chirp-media",
-  avatarBucket: "avatars",
-  bannerBucket: "banners",
+  SUPABASE_URL: 'https://db.chirp.com.ar',
+  SUPABASE_ANON_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtkb2hua3B5a3BjbW5veXhoa3RlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwODYxMjgsImV4cCI6MjA5NzY2MjEyOH0.kPxH259iv3RZZxPJWIpqnfwzyRN-DJoXZdf9uNkRO7c',
+  authCallback: window.location.origin + '/auth-callback.html',
+  updatePassword: window.location.origin + '/update-password.html',
+  chirpyEndpoint: 'https://db.chirp.com.ar/functions/v1/chirpy',
+  mediaBucket: 'chirp-media',
+  avatarBucket: 'avatars',
+  bannerBucket: 'banners',
   chirpLimit: 280,
-  authTimeoutMs: 90000
+  authTimeoutMs: 90000,
 };
